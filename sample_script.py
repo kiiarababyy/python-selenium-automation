5a37d590ddd74f559b611674c9be5b25d5ff1013
kiarah hernandez <kiiarababyy@aol.com>
2023-05-02 15:12:03 -0400

    Homework 1

--- sample_script.py
+++ sample_script.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
 
+
 # get the path to the ChromeDriver executable
 driver_path = ChromeDriverManager().install()
 
@@ -18,7 +19,7 @@ driver.get('https://www.google.com/')
 # populate search field
 search = driver.find_element(By.NAME, 'q')
 search.clear()
-search.send_keys('Car')
+search.send_keys('table')
 
 # wait for 4 sec
 sleep(4)
@@ -27,7 +28,7 @@ sleep(4)
 driver.find_element(By.NAME, 'btnK').click()
 
 # verify search results
-assert 'car' in driver.current_url.lower(), f"Expected query not in {driver.current_url.lower()}"
+assert 'table' in driver.current_url.lower(), f"Expected query not in {driver.current_url.lower()}"
 print('Test Passed')
 
 driver.quit()
